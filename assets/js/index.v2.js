fe208041dbf9f6454d1cef83577a3dd4033fc2a5
Vineet Singh <vwsingh@outlook.com>
2020-12-10 15:52:12 +1100

    added required to date field

--- assets/js/index.v2.js
+++ assets/js/index.v2.js
@@ -17,12 +17,31 @@ function main(){
     //get form
     const form = document.getElementById("taskForm");
     //set date attributes
-    document.getElementById("dDate").setAttribute("min", cDate);
-    document.getElementById("dDate").setAttribute("value", cDate);
+    const dateFld =  document.getElementById("dDate");
+    dateFld.setAttribute("min", cDate);
+    dateFld.setAttribute("value", cDate);
     //add event listener
     form.addEventListener('submit', event => {
+        /*check value of date field if it is blank or in the past - raise error
+        console.log(dateFld.value);
+        if (dateFld.value==="" || Date(dateFld.value) < Date()) {
+            dateFld.classList.add("is-invalid");
+            if (!isVisible(document.getElementById("invalidDateMsg"))){
+                document.getElementById("invalidDateMsg").classList.add("d-block");
+            }
+        }*/ 
         event.preventDefault();
         form.classList.add('was-validated');
     }, false);
 };
+
+/* check if an element is visible on a page */
+function isVisible (ele) {
+    var style = window.getComputedStyle(ele);
+    return (style.width !== "0" &&
+    style.height !== "0" &&
+    style.opacity !== "0" &&
+    style.display!=='none' &&
+    style.visibility!== 'hidden');
+}
 
