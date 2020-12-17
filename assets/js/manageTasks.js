12f98d42b9795c59b9d9f4d0ee832821dc24e5aa
Vineet Singh <vinnie.singh@gmail.com>
2020-12-18 07:34:48 +1100

    added manageTasks.html/js

---
+++ assets/js/manageTasks.js
@@ -0,0 +1,11 @@
+function main() {
+
+}
+
+function renderCards() {
+    const cardRow = document.querySelector("#cardRow");
+    //for each task make a card and display it
+    store.taskList.forEach(ele => {
+
+    });
+}
