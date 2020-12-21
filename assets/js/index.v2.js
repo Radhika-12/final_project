55d34748734d14894db51f6fbf924a8298227f00
Vineet Singh <vwsingh@outlook.com>
2020-12-21 16:50:11 +1100

    finished sprint 2 task 6 vws

--- assets/js/index.v2.js
+++ assets/js/index.v2.js
@@ -5,7 +5,7 @@
     Task: 6
     Author: Vineet W. Singh 
     Start Date: 10/12/2020
-    Date of last edit: 17/12/2020
+    Date of last edit: 21/12/2020
     Date of last review:
 */
 
@@ -25,9 +25,12 @@ function main(){
     const dateFld =  document.getElementById("dDate");
     dateFld.setAttribute("min", cDate);
     dateFld.setAttribute("value", cDate);
-
-    if (store.taskList.length!=0) renderTask();
-
+    // if list of tasks is not empty render list of tasks on screen and set up 
+    // modal that allows modification
+    if (store.taskList.length!=0){
+        renderTask();
+        initModal();
+    }
     //add event listeners, submit & reset
     form.addEventListener('submit', event => {
         event.preventDefault();
@@ -35,6 +38,7 @@ function main(){
         // check if any form-controls have form-control:invalid class, stop if true
         // otherwise add the task to the store.taskList and render on screen.
         if (form.checkValidity()){
+            const listLength=store.taskList.length;
             //no invalid controls detected - continue:
             const projectName = document.querySelector("#projNam").value;
             const taskName = document.querySelector("#taskNam").value;
@@ -43,6 +47,8 @@ function main(){
             const status = document.querySelector("#status").value; 
             const dueDate = document.querySelector("#dDate").value;
             store.addTask(projectName, taskName, desc, assignee, status, dueDate);
+            //check if task list is empty before adding a task and initModal only once
+            if (listLength===0) initModal();
             form.classList.remove("was-validated");
             renderTask(form);
             form.reset();
@@ -53,8 +59,6 @@ function main(){
         form.classList.remove("was-validated");
         form.reset();
     });
-
-
 };
 
 /* check if an element is visible on a page */
@@ -80,6 +84,8 @@ function renderTask(){
         store.taskList.forEach(ele=>{
             // create a new table row
             const tbRow=document.createElement("TR");
+            tbRow.setAttribute("id",`${ele.allData["ID"]}`);
+            tbRow.classList.add("taskTableRow");
             // get all data values from task and add it to a row
             for (prop in ele.allData){
                 //create a new table row data field - task name
@@ -100,6 +106,86 @@ function renderTask(){
         });
     }
 }
+
+// function to initialise a modal which will allow a task to be modified
+function initModal(){
+     //constants used for modal 
+     const modalContHeader = 
+     `<!-- Modal Header -->
+     <div class="modal-header">
+         <h4 class="modal-title">Modal Heading</h4>
+         <button type="button" class="close" data-dismiss="modal">&times;</button>
+     </div>`;
+     let modalContBody = 
+     `<!-- Modal Body -->
+     <div class="modal-body">
+     </div>`;
+     const modalContFooter = 
+     `<!-- Modal Footer -->
+     <div class="modal-footer">
+         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+     </div>`;
+     // make a constant with modal contents
+    const modalCont=modalContHeader+modalContBody+modalContFooter;
+    // initialize on a <div class="modal"> with all options
+    // Note: options object is optional
+    const taskModalInstance = new BSN.Modal(
+        '#taskModal', 
+        // target selector
+        { // options object
+        content: modalCont, // sets modal content
+        backdrop: 'static', // we don't want to dismiss Modal when Modal or backdrop is the click event target
+        keyboard: false // we don't want to dismiss Modal on pressing Esc key
+        }
+    ); //end taskModalInstance
+    //update modal body with task details and set up a "mark as done" button
+    const updateModalBody = id => {
+        const cTask = store.getTaskById(id);
+        const preButtonBody=`<!-- Modal Body -->
+        <div class="modal-body">
+            <div class="text-center">
+                <h4> Task ID: ${cTask.id} </h4>
+                <hr/>
+                <p> Task: ${cTask.taskName} </p>
+                <p> Assigned to: ${cTask.assignee} </p>
+                <p> Status: ${cTask.status} </p>`;
+        const postButtonBody=`<hr /> 
+                <p> To delete the task go to the Manage Tasks Page </p> 
+            </div>
+        </div>`;
+        modalContBody = cTask.status==="done" ? 
+            preButtonBody
+            + `<button type='button' class='btn btn-primary modalBtn' id='${cTask.id}' disabled> Mark as Done </button>`
+            + postButtonBody :
+            preButtonBody
+            + `<button type='button' class='btn btn-primary modalBtn' id='${cTask.id}'> Mark as Done </button>`
+            + postButtonBody ;
+        const modalCont=modalContHeader+modalContBody+modalContFooter;
+        taskModalInstance.setContent(modalCont);
+    }; //end updateModalBody function 
+    // add an event listener for double clicks on the list of tasks
+    document.querySelector("#taskTable").addEventListener("dblclick",event=>
+    {     
+        const element = event.target.closest('.taskTableRow'); 
+        const id = Number(element.getAttribute("id"));
+        updateModalBody(id);
+        //add an event listener for the modal 'mark as done' button
+        document.querySelector(".modalBtn").addEventListener("click",event=>{
+            const element = event.target;
+            const id2 = Number(element.getAttribute("id"));
+            //modify task status in store
+            store.modifyTaskStatus(id2,"done");
+            // disable the 'mark as done' button
+            element.setAttribute("disabled","");
+            //toggle off the modal
+            taskModalInstance.toggle();
+            //refresh the task list to re-render the list on page
+            this.renderTask();
+        });//end event listener for the modal 'mark as done' button
+        //turn on the modal 
+        taskModalInstance.toggle();
+    });//end event listener for the double click on list of tasks
+}//end initModal()
 
 /* custom code for date check: 
     //check value of date field if it is blank or in the past - raise error
